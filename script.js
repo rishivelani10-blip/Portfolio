994fe2ade5b20965220f8bef5376f03a244c6e2b
rishivelani10-blip <rishivelani69@gmail.com>
2026-08-09 13:21:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,100 @@
+// DARK / LIGHT MODE
+
+const themeBtn =
+    document.getElementById("themeBtn");
+
+themeBtn.addEventListener(
+    "click",
+    function () {
+
+        document.body.classList.toggle("light");
+
+        if (
+            document.body.classList.contains("light")
+        ) {
+
+            themeBtn.textContent = "☀️";
+
+        } else {
+
+            themeBtn.textContent = "🌙";
+
+        }
+
+    }
+);
+
+
+// CONTACT FORM
+
+const contactForm =
+    document.getElementById("contactForm");
+
+contactForm.addEventListener(
+    "submit",
+    function (event) {
+
+        event.preventDefault();
+
+        alert(
+            "Thank you! Your message has been submitted."
+        );
+
+        contactForm.reset();
+
+    }
+);
+
+
+// ACTIVE NAVIGATION
+
+const sections =
+    document.querySelectorAll("section");
+
+const navLinks =
+    document.querySelectorAll(".nav-links a");
+
+window.addEventListener(
+    "scroll",
+    function () {
+
+        let current = "";
+
+        sections.forEach(
+            function (section) {
+
+                const sectionTop =
+                    section.offsetTop - 150;
+
+                if (
+                    window.scrollY >= sectionTop
+                ) {
+
+                    current =
+                        section.getAttribute("id");
+
+                }
+
+            }
+        );
+
+        navLinks.forEach(
+            function (link) {
+
+                link.style.color = "";
+
+                if (
+                    link.getAttribute("href")
+                    === "#" + current
+                ) {
+
+                    link.style.color =
+                        "#38bdf8";
+
+                }
+
+            }
+        );
+
+    }
+);
